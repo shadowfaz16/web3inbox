4e54c7d348fc4cbe5838b354f2cfde6c97472096
shadowfaz16 <erikfazju@gmail.com>
2023-09-22 19:47:56 -0400

    github link change

--- components/core/Footer.tsx
+++ components/core/Footer.tsx
@@ -17,7 +17,7 @@ function Footer() {
           rel="noopener noreferrer"
           variant="outline"
           icon={<FaGithub />}
-          href="https://github.com/WalletConnect/gm-hackers#gm-hackers-web3inbox-nextjs-wagmi-react-typescript"
+          href="https://github.com/shadowfaz16"
         />
         <IconButton
           aria-label="toggle theme"
